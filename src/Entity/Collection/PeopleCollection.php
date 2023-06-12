3bbba4666da29209669a62c3e02885e4e2520720
Raphaël DURAND <durandraphael.courriel@gmail.com>
2023-06-12 15:52:16 +0200

    Ajout de la classe PeopleCollection.php

---
+++ src/Entity/Collection/PeopleCollection.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\People;
+use PDO;
+
+class PeopleCollection
+{
+    /**
+     * Classe PeopleCollection. Elle n'est décrite par aucun attribut et n'execute qu'un seul savoir-faire
+     * findByMovieId.
+     */
+
+
+    /**
+     * Méthode de classe PeopleCollection. Elle permet de récupérer un tableau contenant tous les acteurs d'un film
+     * grace à son movieId.
+     * @return People[] un tableau contenant tous les acteurs triés par nom
+     */
+    public static function findByMovieId(int $movieId): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+    SELECT DISTINCT p.id AS "id", avatarId, birthday, deathday, name, biography, placeOfBirth
+    FROM people p
+        JOIN cast c ON p.id=c.peopleId
+    WHERE id = :movieId
+    ORDER BY name ASC
+SQL
+        );
+
+        $stmt->execute(["movieId" =>$movieId]);
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS, People::class);
+    }
+}
