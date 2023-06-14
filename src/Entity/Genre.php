f3c1f9e5c0c2b19b86dd0e2279fd158407216467
Alexandre Philippe <alexandre.philippe@etudiant.univ-reims.fr>
2023-06-14 21:00:26 +0200

    Ajout de la méthode findById dans genre

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
 class Genre
 {
     /** La classe genre est composée de 2 attributs : id et name avec leur getter respectif.
-* @var int
+     * @var int
      */
     private int $id;
     private string $name;
@@ -28,5 +32,26 @@ class Genre
         return $this->name;
     }
 
+    /** Méthode de la classe genre. Elle permet de trouver un genre à partir de son id.
+     * @param int $id
+     * @return mixed
+     */
+    public function findById(int $id): Genre
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<SQL
+        SELECT id, name
+        FROM genre
+        WHERE id= :id
+SQL
+        );
+        $stmt->execute(["id" => $id]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Genre::class);
+        if (($result = $stmt->fetch()) !== false) {
+            return $result;
+        } else {
+            throw new EntityNotFoundException("Le genre demandé est introuvable");
+        }
+    }
 
 }
